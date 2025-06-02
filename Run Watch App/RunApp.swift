38eb640a0b40f51312e17fc7214eec6a52609ca3
SuperHuzifei <381418077@qq.com>
2025-06-03 03:49:03 +0800

    加入watchos 模板

---
+++ Run Watch App/RunApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunApp.swift
+//  Run Watch App
+//
+//  Created by 胡云飞 on 2025/6/3.
+//
+
+import SwiftUI
+
+@main
+struct Run_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
