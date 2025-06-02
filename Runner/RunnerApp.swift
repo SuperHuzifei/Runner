2c8743f21e139a883e9e2843fc2aaa197000cd27
SuperHuzifei <381418077@qq.com>
2025-06-02 14:58:40 +0800

    Initial Commit

---
+++ Runner/RunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnerApp.swift
+//  Runner
+//
+//  Created by 胡云飞 on 2025/6/2.
+//
+
+import SwiftUI
+
+@main
+struct RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
